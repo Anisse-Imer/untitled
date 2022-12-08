c80ff377a791fad98268606314433de959430496
Anisse <anissezou@gmail.com>
2022-12-08 15:45:58 +0100

    Modification des throws et correction des booleans dans PileD et PileS.

--- src/main/java/LIFO/Statique/UtilisationPileS.java
+++ src/main/java/LIFO/Statique/UtilisationPileS.java
@@ -1,5 +1,9 @@
 package LIFO.Statique;
 public class UtilisationPileS {
+
+    public void DATAOUT(){
+
+    }
     public static void main(String[] args) {
         try {
             PileS p = new PileS(4);
@@ -8,16 +12,12 @@ public class UtilisationPileS {
             p.Push(7);
             p.Push(2);
             p.Push(7);
-            p.Push(7);
-            p.Push(7);
             System.out.println(p.toString());
         } catch (Exception e){
             System.out.println(e);
         }
-
         try {
             PileD d = new PileD();
-            System.out.println(d.toString());
             d.Push("La ");
             d.Push("sauvagerie ");
             d.Push("au micro");
